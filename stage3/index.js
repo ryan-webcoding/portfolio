3c7a896a1a43637b1b6a99704e166089c84f1c5c
ryanluo <zhengyangluo180@gmail.com>
2025-03-04 19:31:09 +1100

    rearrange file

--- stage3/index.js
+++ stage3/index.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const img = document.getElementById("laptop");
 
   img.addEventListener("mouseenter", () => {
-    img.src = "./laptop_on.png";
+    img.src = "./img/laptop_on.png";
   });
 
   img.addEventListener("mouseleave", () => {
-    img.src = "./laptop.png";
+    img.src = "./img/laptop.png";
   });
 
   img.addEventListener("click", () => {
