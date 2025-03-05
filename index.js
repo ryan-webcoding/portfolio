1f3280a5714c01dad14dac3c6ee139b39c143747
ryanluo <zhengyangluo180@gmail.com>
2025-03-05 17:50:42 +1100

    add easter egg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const noButton = document.querySelector(".no");
   const yesButton = document.querySelector(".yes");
+  let noClickCount = 0;
 
   function addSpinEffect(element) {
     element.addEventListener("mouseenter", () => {
@@ -18,7 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
   addSpinEffect(yesButton);
 
   noButton.addEventListener("click", () => {
-    window.location.href = "https://www.google.com";
+    noClickCount++;
+
+    switch (noClickCount) {
+      case 1:
+        alert("How dare you");
+        break;
+      case 2:
+        alert("I warned you!");
+        break;
+      case 3:
+        alert("Please dont, you'll regret it.");
+        break;
+      case 4:
+        window.location.href = "https://www.google.com";
+        break;
+      default:
+        break;
+    }
   });
 
   yesButton.addEventListener("click", () => {
